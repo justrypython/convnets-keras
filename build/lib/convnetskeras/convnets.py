c58f77dfaa80edb90966c3339205e1373a416ea0
kerzhao <kerzhao@163.com>
2017-08-04 08:13:18 +0000

    update

--- build/lib/convnetskeras/convnets.py
+++ build/lib/convnetskeras/convnets.py
@@ -95,6 +95,8 @@ def __get_model_based_on_input_network(network):
         convnet_init = VGG_19
     elif network == 'alexnet':
         convnet_init = AlexNet
+    elif network == 'alexnet_227':
+        convnet_init = AlexNet_227
     else:
         raise ValueError("Only 'vgg_16', 'vgg_19', 'alexnet' models available")
     return convnet_init
@@ -232,6 +234,64 @@ def AlexNet(weights_path=None, heatmap=False):
     if heatmap:
         inputs = Input(shape=(3, None, None))
     else:
+        inputs = Input(shape=(3, 100, 100))
+
+    conv_1 = Convolution2D(96, 11, 11, subsample=(4, 4), activation='relu',
+                           name='conv_1')(inputs)
+
+    conv_2 = MaxPooling2D((3, 3), strides=(2, 2))(conv_1)
+    conv_2 = crosschannelnormalization(name='convpool_1')(conv_2)
+    conv_2 = ZeroPadding2D((2, 2))(conv_2)
+    conv_2 = merge([
+                       Convolution2D(128, 5, 5, activation='relu', name='conv_2_' + str(i + 1))(
+                           splittensor(ratio_split=2, id_split=i)(conv_2)
+                       ) for i in range(2)], mode='concat', concat_axis=1, name='conv_2')
+
+    conv_3 = MaxPooling2D((3, 3), strides=(2, 2))(conv_2)
+    conv_3 = crosschannelnormalization()(conv_3)
+    conv_3 = ZeroPadding2D((1, 1))(conv_3)
+    conv_3 = Convolution2D(384, 3, 3, activation='relu', name='conv_3')(conv_3)
+
+    conv_4 = ZeroPadding2D((1, 1))(conv_3)
+    conv_4 = merge([
+                       Convolution2D(192, 3, 3, activation='relu', name='conv_4_' + str(i + 1))(
+                           splittensor(ratio_split=2, id_split=i)(conv_4)
+                       ) for i in range(2)], mode='concat', concat_axis=1, name='conv_4')
+
+    conv_5 = ZeroPadding2D((1, 1))(conv_4)
+    conv_5 = merge([
+                       Convolution2D(128, 3, 3, activation='relu', name='conv_5_' + str(i + 1))(
+                           splittensor(ratio_split=2, id_split=i)(conv_5)
+                       ) for i in range(2)], mode='concat', concat_axis=1, name='conv_5')
+
+    dense_1 = MaxPooling2D((3, 3), strides=(2, 2), name='convpool_5')(conv_5)
+
+    if heatmap:
+        dense_1 = Convolution2D(4096, 2, 2, activation='relu', name='dense_1')(dense_1)
+        dense_2 = Convolution2D(4096, 1, 1, activation='relu', name='dense_2')(dense_1)
+        dense_3 = Convolution2D(3, 1, 1, name='dense_3')(dense_2)
+        prediction = Softmax4D(axis=1, name='softmax')(dense_3)
+    else:
+        dense_1 = Flatten(name='flatten')(dense_1)
+        dense_1 = Dense(4096, activation='relu', name='dense_1')(dense_1)
+        dense_2 = Dropout(0.5)(dense_1)
+        dense_2 = Dense(4096, activation='relu', name='dense_2')(dense_2)
+        dense_3 = Dropout(0.5)(dense_2)
+        dense_3 = Dense(3, name='dense_3')(dense_3)
+        prediction = Activation('softmax', name='softmax')(dense_3)
+
+    model = Model(input=inputs, output=prediction)
+
+    if weights_path:
+        model.load_weights(weights_path)
+
+    return model
+
+
+def AlexNet_227(weights_path=None, heatmap=False):
+    if heatmap:
+        inputs = Input(shape=(3, None, None))
+    else:
         inputs = Input(shape=(3, 227, 227))
 
     conv_1 = Convolution2D(96, 11, 11, subsample=(4, 4), activation='relu',
@@ -282,6 +342,17 @@ def AlexNet(weights_path=None, heatmap=False):
 
     if weights_path:
         model.load_weights(weights_path)
+        
+    lastlayer = model.layers[-2].output
+    if heatmap:
+        dense_4 = Convolution2D(2, 1, 1, name='dense_4')(lastlayer)
+        prediction = Softmax4D(axis=1, name='softmax')(dense_4)
+    else:
+        dense_4 = Dropout(0.5)(lastlayer)
+        dense_4 = Dense(2, name='dense_4')(dense_4)
+        prediction = Activation('softmax', name='softmax')(dense_4)
+        
+    model = Model(input=inputs, output=prediction)
 
     return model
 
@@ -354,3 +425,4 @@ def _demo_heatmap_script():
 
 if __name__ == '__main__':
     _demo_heatmap_script()
+
